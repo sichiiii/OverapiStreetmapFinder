74bf97fbcf63824c13392cba6636406848c603ad
fisyukovich24@gmail.com <kirillfisyukovich@gmail.com>
2021-12-06 17:39:47 +0300

    final 1.1

--- argvs.py
+++ argvs.py
@@ -196,7 +196,7 @@ class Overpy_map():
         
         random.shuffle(res)
         temp = 0
-        while 1:
+        for i in range(0, len(res)):
             try:
                 # https://www.google.com/maps/place/12+Vla+d'Est%C3%A9,+75013+Paris/@48.8221615,2.3648274,17z/data=!3m1!4b1!4m5!3m4!1s0x47e67229a6ca577f:0xc13c63b0b6802c32!8m2!3d48.8221615!4d2.3670161
                 res[temp].tags['link'] = f"""https://www.google.com/maps/place/{res[temp].tags['addr:housenumber']}+{
@@ -205,10 +205,10 @@ class Overpy_map():
                 res[temp].tags['link'] = res[temp].tags['link'].replace(' ', '+')
                 arr.append(res[temp].tags)
                 temp +=1
-                print(res[temp].tags['link'] )
+                print(res[temp].tags['link'])
             except Exception as ex:
                 print(temp)
-                break
+                continue
         print(arr)
         return arr
     except Exception as ex:
